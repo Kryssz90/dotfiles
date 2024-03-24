c6618535af7107428c15f80280507c1a57bc8d25
Krisztián Zsobrák <kryssz90@gmail.com>
2024-03-24 21:48:27 +0100

    Bugfixes

--- run_onchange_20_install-fonts.sh
+++ run_onchange_20_install-fonts.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+tmpdir=$(mktemp -d)
+cd $tmpdir
 wget "https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip"
 unzip CascadiaCode-2111.01.zip
 mkdir -p ~/.local/share/fonts
 mv ttf/Cascadia* ~/.local/share/fonts/
+cd ..
+rm -rf $tmpdir
--- run_onchange_90_configure.sh
+++ run_onchange_90_configure.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 echo "Set default window manager"
-# sudo update-alternatives --set x-session-manager /usr/bin/i3
+sudo update-alternatives --set x-session-manager /usr/bin/i3
 
 echo "Set default terminal"
-# sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /snap/bin/alacritty 50
+sudo update-alternatives --config x-terminal-emulator
 
 echo "Set default shell"
 chsh -s /usr/bin/zsh
