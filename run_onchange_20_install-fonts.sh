b7515e23c1638504b4f8c2585e1729da4cc94fa9
Krisztián Zsobrák <krisztian.zsobrak@vipps.no>
2024-03-25 19:28:00 +0100

    Update font settings

--- run_onchange_20_install-fonts.sh
+++ run_onchange_20_install-fonts.sh
@@ -8,3 +8,14 @@ mkdir -p ~/.local/share/fonts
 mv ttf/Cascadia* ~/.local/share/fonts/
 cd ..
 rm -rf $tmpdir
+
+
+sudo apt install fontconfig
+cd ~
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d .local/share/fonts
+cd .local/share/fonts
+rm *Windows*
+cd ~
+rm Meslo.zip
+fc-cache -fv
