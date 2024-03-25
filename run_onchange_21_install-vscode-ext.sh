565bbb65e792750a04c274893c9303638166d41d
Krisztián Zsobrák <krisztian.zsobrak@vipps.no>
2024-03-25 20:33:48 +0100

    Add VSCODE settings

---
+++ run_onchange_21_install-vscode-ext.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+echo "Installing vscode extensions"
+# To export list of extensions:
+# code --list-extensions | xargs -L 1 echo code --install-extension > ~/privat/dotfiles/ext.list
+
+code --install-extension 42crunch.vscode-openapi
+code --install-extension adamhartford.vscode-base64
+code --install-extension alefragnani.bookmarks
+code --install-extension bierner.markdown-mermaid
+code --install-extension darkriszty.markdown-table-prettify
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension firsttris.vscode-jest-runner
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+code --install-extension github.github-vscode-theme
+code --install-extension github.vscode-pull-request-github
+code --install-extension golang.go
+code --install-extension graphql.vscode-graphql
+code --install-extension graphql.vscode-graphql-execution
+code --install-extension graphql.vscode-graphql-syntax
+code --install-extension htmlhint.vscode-htmlhint
+code --install-extension lixquid.calculator
+code --install-extension lokalise.i18n-ally
+code --install-extension mhutchie.git-graph
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.isort
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
+code --install-extension ms-vscode.hexeditor
+code --install-extension ms-vscode.makefile-tools
+code --install-extension mtxr.sqltools
+code --install-extension mtxr.sqltools-driver-mssql
+code --install-extension nick-rudenko.back-n-forth
+code --install-extension orta.vscode-jest
+code --install-extension pkief.material-icon-theme
+code --install-extension rangav.vscode-thunder-client
+code --install-extension redhat.vscode-yaml
+code --install-extension richie5um2.vscode-statusbar-json-path
+code --install-extension ritwickdey.liveserver
+code --install-extension rvest.vs-code-prettier-eslint
+code --install-extension simonsiefke.svg-preview
+code --install-extension stylelint.vscode-stylelint
+code --install-extension weijunyu.vscode-json-path
+code --install-extension yokawasa.jwt-debugger
+code --install-extension zignd.html-css-class-completion
