1ce9d640aab5ebba6070b4805a676ad788c53a70
Krisztián Zsobrák <kryssz90@gmail.com>
2024-05-10 23:39:45 +0200

    Fix

--- run_onchange_24_setup_nvim.sh
+++ run_onchange_24_setup_nvim.sh
@@ -1,12 +1,12 @@
-#/bin/sh
+#!/bin/sh
 
 # Back up the current configuration
 echo "Backing up the current configuration"
 mkdir -p ~/.config/nvim_backup
 mkdir -p ~/.local/share/nvim_backup
 
-cp -r ~/.config/nvim/* ~/.config/nvim_backup
-cp -r ~/.local/share/nvim/* ~/.local/share/nvim_backup
+cp -r ~/.config/nvim ~/.config/nvim_backup
+cp -r ~/.local/share/nvim ~/.local/share/nvim_backup
 
 # Remove the current configuration
 echo "Removing the current configuration"
@@ -19,8 +19,10 @@ git clone https://github.com/NvChad/starter ~/.config/nvim
 
 # Restore the backup, overwriting the new configuration
 echo "Restoring the backup"
-cp -rf ~/.config/nvim_backup/* ~/.config/nvim
-cp -rf ~/.local/share/nvim_backup/* ~/.local/share/nvim
+mkdir -p ~/.config/nvim
+mkdir -p ~/.local/share/nvim
+cp -rf ~/.config/nvim_backup ~/.config/nvim
+cp -rf ~/.local/share/nvim_backup ~/.local/share/nvim
 
 # Remove backup
 rm -rf ~/.config/nvim_backup
